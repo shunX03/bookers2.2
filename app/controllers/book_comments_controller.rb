189481da8369b2617e020eaaf37008e24a8e77d1
EC2 Default User <you@example.com>
2022-08-08 09:48:04 +0000

    Initial Commit

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -6,12 +6,12 @@ class BookCommentsController < ApplicationController
     comment.user_id = current_user.id
     comment.book_id = book.id
     comment.save
-    redirect_to books_path(book)
+    redirect_to request.referer
  end
 
   def destroy
     BookComment.find(params[:id]).destroy
-    redirect_to book_path(params[:book_id])
+    redirect_to request.referer
   end
 
   private
