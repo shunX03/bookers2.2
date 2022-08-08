9a19b5c3ee999eeea244fe3267a7d654550b3261
EC2 Default User <you@example.com>
2022-08-08 12:13:13 +0000

    Initial Commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,6 +27,20 @@ class User < ApplicationRecord
   def following?(user)
   following_user.include?(user)
   end
+  
+  def self.looks(search, word)
+    if search == "perfect_match"
+      @user = User.where("name LIKE?", "#{word}")
+    elsif search == "forward_match"
+      @user = User.where("name LIKE?","#{word}%")
+    elsif search == "backward_match"
+      @user = User.where("name LIKE?","%#{word}")
+    elsif search == "partial_match"
+      @user = User.where("name LIKE?","%#{word}%")
+    else
+      @user = User.all
+    end
+  end
 
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
